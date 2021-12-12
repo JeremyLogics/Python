d0464b0fb789f7ba1e261104607563dd5874c62d
Jeremy <49837200+JeremyLogics@users.noreply.github.com>
2021-12-12 08:48:53 -0600

    try/except method

--- #basics.py
+++ #basics.py
@@ -11,19 +11,23 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days):
-    if num_of_days > 0: 
         return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
-    elif num_of_days == 0: 
-        return "Please enter a positive number, not zero."
+
 
 
 def validate_and_execute():
-    if user_input.isdigit():
-        #user_input_number = int(user_input)
-        calculated_value = days_to_units(int(user_input))
-        print(calculated_value)
-    else:
-        print ("your input is not a valid number, dont ruin my program!")
+    try:
+
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print ("Please enter a positive number, not zero.")
+        else: 
+            print ("You entered a negative number, no conversion for you!")
+    except ValueError:
+        print ("Your input is not a valid number, dont ruin my program!")
 
 user_input = input("Enter number of days:\n")
 validate_and_execute()
